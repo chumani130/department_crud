b873b2c84f8b644439546c1e79e8d705c8fd5e05
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-05-10 10:36:32 +0200

    Initial commit

---
+++ src/test/java/com/chumz/department_crud/DepartmentCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chumz.department_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
