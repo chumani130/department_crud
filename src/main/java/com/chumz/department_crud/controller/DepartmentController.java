0c9be36a84eeeff070cbaac10a9b8b004f09bf04
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-05-10 12:01:58 +0200

    Update DepartmentController.java

--- src/main/java/com/chumz/department_crud/controller/DepartmentController.java
+++ src/main/java/com/chumz/department_crud/controller/DepartmentController.java
@@ -1,4 +1,40 @@
 package com.chumz.department_crud.controller;
 
+import com.chumz.department_crud.entity.DepartmentEntity;
+import com.chumz.department_crud.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class DepartmentController {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    // save operation
+    @GetMapping("/departments")
+    public DepartmentEntity saveDepartment(@Validated @RequestBody DepartmentEntity departmentEntity) {
+        return departmentService.saveDepartment(departmentEntity);
+    }
+
+    // Read operation
+    @GetMapping("/departments/{id}")
+    public List<DepartmentEntity> fetchDepartmentList() {
+        return departmentService.fetchDepartList();
+    }
+
+    // update operation
+    @PutMapping("/departments/{id}")
+    public DepartmentEntity updateDepartment(@RequestBody DepartmentEntity departmentEntity, @PathVariable("id") Long departmentId) {
+        return departmentService.updateDepartment(departmentEntity, departmentId);
+    }
+
+    //delete operation
+    @DeleteMapping("departments/{id}")
+    public String deleteDepartmentById(@PathVariable("id") Long departmentId) {
+        return "Deleted successfully";
+    }
 }
