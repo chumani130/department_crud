4de07990499085643de9afff80fbfb0c7dce2f5a
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-05-10 14:46:43 +0200

    Update DepartmentRepository.java

--- src/main/java/com/chumz/department_crud/repository/DepartmentRepository.java
+++ src/main/java/com/chumz/department_crud/repository/DepartmentRepository.java
@@ -1,9 +1,10 @@
 package com.chumz.department_crud.repository;
 
 import com.chumz.department_crud.entity.DepartmentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DepartmentRepository extends CrudRepository<DepartmentEntity, Long> {
+public interface DepartmentRepository extends JpaRepository<DepartmentEntity, Long> {
 }
